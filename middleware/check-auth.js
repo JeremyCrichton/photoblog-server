d05f1ebfc48b1266f0807150436dfdc367269b85
Jeremy Crichton <you@example.com>
2020-01-06 23:36:54 +0800

    add route protection

---
+++ middleware/check-auth.js
@@ -0,0 +1,33 @@
+/**
+ * Validate an incoming request from its token
+ */
+
+const jwt = require('jsonwebtoken');
+const HttpError = require('../models/http-error');
+
+module.exports = (req, res, next) => {
+  // extract token from incoming request header
+  // headers property is provided by request & contains kv pairs
+
+  // 2 possible errors here
+  // 1. try-catch looking for if auth header hasn't been set, as trying to run split method would fail
+  // 2. if (!token) looking for if whatever's in the auth header doesn't give us a token
+  try {
+    const token = req.headers.authorization.split(' ')[1]; // Authorization: 'Bearer TOKEN'
+    if (!token) {
+      throw new Error('Authentication failed!');
+    }
+    // Validate token
+    // if this validation fails, it will throw an error and thus make it to the catch block
+    const decodedToken = jwt.verify(token, 'supersecret_dont_share');
+
+    // Add data to the request
+    req.userData = { userId: decodedToken.userId };
+
+    // Allow request to continue
+    next();
+  } catch (err) {
+    // return to ensure no code after this executes
+    return next(new HttpError('Authentication failed!', 401));
+  }
+};
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -3,12 +3,25 @@ const { check } = require('express-validator');
 
 const placesControllers = require('../controllers/places-controller');
 const fileUpload = require('../middleware/file-upload');
+const checkAuth = require('../middleware/check-auth');
 
 const router = express.Router();
 
 router.get('/:pid', placesControllers.getPlaceById);
 
 router.get('/user/:uid', placesControllers.getPlacesByUserId);
+
+/**
+ *  ROUTE PROTECTION
+ * - Requests travel through middlewares from top to bottom
+ * - The above routes are accessible by any request
+ * - Here we create a middleware to check for a valid token
+ *   - if invalid, block the request and stop request from continuing to other routes
+ */
+
+// Don't execute checkAuth, just pass a pointer to use so checkAuth gets registered as a middleware
+// all routes after this can only be accessed w/ a valid token
+router.use(checkAuth);
 
 // Any post request to /api/places
 // Middlewares (parameters) are evaluated left to right
